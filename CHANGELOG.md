0f13fb7317df8abbcf44b9fa0fb095aa3e0e278c
Manuel Marquez <buzondemam@gmail.com>
2018-02-14 16:02:22 -0400

    [FIX] Remove call to method _get_delivery_methods() original

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+## [9.0.1.0.3] 2018-02-14
+### changed
+- Fix: Remove call to method _get_delivery_methods() original.
+
 ## [9.0.1.0.2] 2018-02-13
 ### changed
 - Fix: Calls to method _get_delivery_carrier_id() passing the available carriers ids.
--- models/sale_order.py
+++ models/sale_order.py
@@ -9,10 +9,20 @@ class SaleOrder(models.Model):
     @api.model
     def _get_delivery_methods(self, order):
 
-        available_carrier_ids = super(
-            SaleOrder, self)._get_delivery_methods(order)
+        sql = ('SELECT id '
+               'FROM delivery_carrier '
+               'WHERE website_published=true')
+        self.env.cr.execute(sql)
+
+        delivery_carriers_ids = [
+            dc[0]
+            for dc in self.env.cr.fetchall()
+            if dc
+        ]
+
         carrier = order._get_delivery_carrier_id(
-            delivery_carriers_ids=available_carrier_ids)
+            delivery_carriers_ids)
+
         if carrier and carrier.id:
             return [carrier.id]
         return []
