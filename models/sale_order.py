d8e6802aa21628f570b124335de8be33ab0e3c8e
Moisés Alejandro Navarro Presas <moisalejandro@gmail.com>
2018-02-23 13:51:59 -0600

    [FIX] iteare only over factible carriers

--- models/sale_order.py
+++ models/sale_order.py
@@ -8,21 +8,17 @@ class SaleOrder(models.Model):
 
     @api.model
     def _get_delivery_methods(self, order):
-
-        sql = ('SELECT id '
-               'FROM delivery_carrier '
-               'WHERE website_published=true')
+        sql = ("SELECT id"
+               " FROM delivery_carrier"
+               " WHERE website_published=true"
+               " AND '" + order.partner_shipping_id.zip + "' <= zip_from"
+               " AND '" + order.partner_shipping_id.zip + "' >= zip_to")
         self.env.cr.execute(sql)
 
-        delivery_carriers_ids = [
-            dc[0]
-            for dc in self.env.cr.fetchall()
-            if dc
-        ]
+        delivery_carriers_ids = [dc[0] for dc in self.env.cr.fetchall() if dc]
 
         carrier = order._get_delivery_carrier_id(
             delivery_carriers_ids)
-
         if carrier and carrier.id:
             return [carrier.id]
         return []
